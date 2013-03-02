ab1876a731fddaaaed81ccdec3872a9f3cacb062
leochen4891 <leochen4891@gmail.com>
2013-03-03 02:20:55 +0800

    after checkstyle

--- Assignment01_Percolation/src/Percolation.java
+++ Assignment01_Percolation/src/Percolation.java
@@ -6,85 +6,89 @@
 
 
 public class Percolation {
-	WeightedQuickUnionUF mUF; 
-	private int mN;
-	private int mNN;
-	private int mVTop;
-	private int mVBot;
-	
-	private int[][] mGrid;
-	
-	// create N-by-N grid, with all sites blocked
-	public Percolation(int N) {
-		mN = N;
-		mNN= N*N;
-		
-		mUF = new WeightedQuickUnionUF(mNN + 2);// 2 more virtual sites
-		
-		mGrid = new int[N][N];
-		for (int i = 0; i < N; i++){ for (int j = 0; j < N; j++) { mGrid[i][j] = 0; } }
-		
-		mVTop = mNN;
-		mVBot = mNN + 1;
-		
-		// link virtual top and bottom sites
-		for (int i = 0; i < N; i++) {
-			mUF.union(mVTop, i);
-			mUF.union(mVBot, mNN - N +i);
-		}
-	}
+    private WeightedQuickUnionUF mUF; 
+    private int mN;
+    private int mNN;
+    private int mVTop;
+    private int mVBot;
+    
+    private int[][] mGrid;
 
-	// open site (row i, column j) if it is not already
-	public void open(int i, int j) {
-		// open a site means add 4 links
-		//         i-1,j
-		//           |
-		// i,j-1 -- i,j -- i,j+1
-		//           |
-		//         i+1,j
-		mGrid[i][j] = 1;
-		if (i-1 >= 0 && 1 == mGrid[i-1][j]) {
-			mUF.union(mN*i+j, mN*(i-1)+j);
-		}
-		if (j-1 >= 0 && 1 == mGrid[i][j-1]) {
-			mUF.union(mN*i+j, mN*i+(j-1));
-		}
-		if (i+1 < mN && 1 == mGrid[i+1][j]) {
-			mUF.union(mN*i+j, mN*(i+1)+j);
-		}
-		if (j+1 < mN && 1 == mGrid[i][j+1]) {
-			mUF.union(mN*i+j, mN*i+(j+1));
-		}
-	}
+    // create N-by-N grid, with all sites blocked
+    public Percolation(int N) {
+        mN = N;
+        mNN = N * N;
 
-	// is site (row i, column j) open?
-	public boolean isOpen(int i, int j) {
-		return (1 == mGrid[i][j]);
-	}
+        mUF = new WeightedQuickUnionUF(mNN + 2); // 2 more virtual sites
 
-	// is site (row i, column j) full?
-	public boolean isFull(int i, int j) {
-		return (mUF.connected(mVTop, mN*i+j));
-	}
+        mGrid = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                mGrid[i][j] = 0;
+            }
+        }
 
-	// does the system percolate?
-	public boolean percolates() {
-		return (mUF.connected(mVTop, mVBot));
-	}
-	
-	private void Print() {
-		String str = "-------------------------------------------------------------\n";
-		for (int i = 0; i < mN; i++) {
-			for (int j = 0; j < mN; j++) {
-				if (mGrid[i][j] == 0)
-					str += "X"; // closed
-				else if (mUF.connected(mVTop, mN*i+j))
-					str += "+"; // full
-				else 
-					str += " "; // empty
-			}
-			str += "\n";
-		}
-		System.out.print(str);
-	}
+        mVTop = mNN;
+        mVBot = mNN + 1;
+
+        // link virtual top and bottom sites
+        for (int i = 0; i < N; i++) {
+            mUF.union(mVTop, i);
+            mUF.union(mVBot, mNN - N + i);
+        }
+    }
+
+    // open site (row i, column j) if it is not already
+    public void open(int i, int j) {
+        // open a site means add 4 links
+        //         i-1,j
+        //           |
+        // i,j-1 -- i,j -- i,j+1
+        //           |
+        //         i+1,j
+        mGrid[i][j] = 1;
+        if (i-1 >= 0 && 1 == mGrid[i-1][j]) {
+            mUF.union(mN*i+j, mN*(i-1)+j);
+        }
+        if (j-1 >= 0 && 1 == mGrid[i][j-1]) {
+            mUF.union(mN*i+j, mN*i+(j-1));
+        }
+        if (i+1 < mN && 1 == mGrid[i+1][j]) {
+            mUF.union(mN*i+j, mN*(i+1)+j);
+        }
+        if (j+1 < mN && 1 == mGrid[i][j+1]) {
+            mUF.union(mN*i+j, mN*i+(j+1));
+        }
+    }
+
+    // is site (row i, column j) open?
+    public boolean isOpen(int i, int j) {
+        return (1 == mGrid[i][j]);
+    }
+
+    // is site (row i, column j) full?
+    public boolean isFull(int i, int j) {
+        return (mUF.connected(mVTop, mN*i+j));
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return (mUF.connected(mVTop, mVBot));
+    }
+    
+    //private void print() {
+    //    String str = "----------------------------------------------------\n";
+    //    for (int i = 0; i < mN; i++) {
+    //        for (int j = 0; j < mN; j++) {
+    //            if (mGrid[i][j] == 0)
+    //                str += "X"; // closed
+    //            else if (mUF.connected(mVTop, mN*i+j))
+    //                str += "+"; // full
+    //            else 
+    //                str += " "; // empty
+    //        }
+    //        str += "\n";
+    //    }
+    //    System.out.print(str);
+    //}
 }
--- Assignment01_Percolation/src/PercolationStats.java
+++ Assignment01_Percolation/src/PercolationStats.java
@@ -1,83 +1,82 @@
-import java.sql.Time;
-
 public class PercolationStats {
-	private int mN;
-	private int mT;
-	private double[] mXs;
-	private double mMean = -1;
-	private double mStddev = -1;
-	
-	// perform T independent computational experiments on an N-by-N grid
-	public PercolationStats(int N, int T) {
-		mN = N;
-		mT = T;
-		mXs = new double[T];
-		int NN = N*N;
-		
-		// try T times
-		StdRandom.setSeed(System.currentTimeMillis());
-		for (int t = 0; t < T; t++) {
-			int i,j, cnt = 0;
-			Percolation per = new Percolation(N);
-			boolean percolation = false;
-			while(cnt < NN && percolation == false) {
-				i = StdRandom.uniform(N);
-				j = StdRandom.uniform(N);
-				if (per.isOpen(i, j))
-					continue;
-				
-				per.open(i, j);
-				cnt++;
-				percolation = per.percolates();
-			}
-			assert(true == percolation);
-			mXs[t] = cnt/(double)NN;
-		}
-	};
+    private int mN;
+    private int mT;
+    private double[] mXs;
+    private double mMean = -1;
+    private double mStddev = -1;
 
-	// sample mean of percolation threshold
-	public double mean() {
-		double total = 0;
-		for (int cnt = 0; cnt < mT; cnt++) {
-			total += mXs[cnt];
-		}
-		mMean = total/(double)mT;
-		return mMean;
-	};
+    // perform T independent computational experiments on an N-by-N grid
+    public PercolationStats(int N, int T) {
+        mN = N;
+        mT = T;
+        mXs = new double[T];
+        int NN = N * N;
 
-	// sample standard deviation of percolation threshold
-	public double stddev() {
-		mStddev = StdStats.stddev(mXs);
-		return mStddev;
-	};
+        // try T times
+        StdRandom.setSeed(System.currentTimeMillis());
+        for (int t = 0; t < T; t++) {
+            int i, j, cnt = 0;
+            Percolation per = new Percolation(N);
+            boolean percolation = false;
+            while (cnt < NN && !percolation) {
+                i = StdRandom.uniform(N);
+                j = StdRandom.uniform(N);
+                if (per.isOpen(i, j))
+                    continue;
 
-	// returns lower bound of the 95% confidence interval
-	public double confidenceLo() {
-		if (mMean < 0)
-			mean();
-		if (mStddev < 0)
-			stddev();
-		return (mMean - 1.96*mStddev/Math.sqrt(mT));
-	};
+                per.open(i, j);
+                cnt++;
+                percolation = per.percolates();
+            }
+            assert (percolation);
+            mXs[t] = cnt / (double) NN;
+        }
+    };
 
-	// returns upper bound of the 95% confidence interval
-	public double confidenceHi() {
-		if (mMean < 0)
-			mean();
-		if (mStddev < 0)
-			stddev();
-		return (mMean + 1.96*mStddev/Math.sqrt(mT));
-	};
+    // sample mean of percolation threshold
+    public double mean() {
+        double total = 0;
+        for (int cnt = 0; cnt < mT; cnt++) {
+            total += mXs[cnt];
+        }
+        mMean = total / (double) mT;
+        return mMean;
+    };
 
-	// test client, described below
-	public static void main(String[] args) {
-		int N = 1000;
-		int T = 5;
-		PercolationStats stats = new PercolationStats(N, T);
-		System.out.print("N = " + N + ", T = " + T + "\n");
-		System.out.print("mean   = " + stats.mean() + "\n");
-		System.out.print("stddev = " + stats.stddev() + "\n");
-		System.out.print("95% CI = " + stats.confidenceLo() + ", " + stats.confidenceHi() + "\n");
-	};
-	
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        mStddev = StdStats.stddev(mXs);
+        return mStddev;
+    };
+
+    // returns lower bound of the 95% confidence interval
+    public double confidenceLo() {
+        if (mMean < 0)
+            mean();
+        if (mStddev < 0)
+            stddev();
+        return (mMean - 1.96 * mStddev / Math.sqrt(mT));
+    };
+
+    // returns upper bound of the 95% confidence interval
+    public double confidenceHi() {
+        if (mMean < 0)
+            mean();
+        if (mStddev < 0)
+            stddev();
+        return (mMean + 1.96 * mStddev / Math.sqrt(mT));
+    };
+
+    // test client, described below
+    public static void main(String[] args) {
+        int N = 200;
+        int T = 100;
+        PercolationStats stats = new PercolationStats(N, T);
+        System.out.print("N = " + N + ", T = " + T + "\n");
+        System.out.print("mean   = " + stats.mean() + "\n");
+        System.out.print("stddev = " + stats.stddev() + "\n");
+        System.out.print("95% CI = " + stats.confidenceLo() + ", "
+                + stats.confidenceHi() + "\n");
+    };
+
 }
